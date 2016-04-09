8e136f5eb6c55bd681e92e0574dd5160755415cb
Yan Knoop <Yan.Knoop@gradsingames.com>
2016-04-09 22:14:40 +0200

    Fix shuuters

--- Nordic16/Nordic16/Assets/Code/Environment/Door.cs
+++ Nordic16/Nordic16/Assets/Code/Environment/Door.cs
@@ -34,20 +34,20 @@ public class Door : MonoBehaviour {
         { 
             float t_RelativeTime = (Time.time - m_StartTime) / m_TimeLength;
             float t_Factor = m_AnimationCurve.Evaluate(t_RelativeTime);
-            float t_Closed = m_HalfDoorLength * 0.5f;
-            float t_Opened = m_HalfDoorLength * 1.5f;
+            float t_Closed = 0.0f;
+            float t_Opened = m_HalfDoorLength * 1.0f;
             
             if (m_IsOpening)
             {
                 float t_CurrentHeight = t_Closed + (t_Opened - t_Closed) * t_Factor;
-                upperdoor.transform.localPosition = transform.up * t_CurrentHeight;
-                lowerdoor.transform.localPosition = -transform.up * t_CurrentHeight;
+                upperdoor.transform.localPosition = Vector3.up * t_CurrentHeight;
+                lowerdoor.transform.localPosition = -Vector3.up * t_CurrentHeight;
             }
             else
             {
                 float t_CurrentHeight = t_Opened + (t_Closed - t_Opened) * t_Factor;
-                upperdoor.transform.localPosition = transform.up * t_CurrentHeight;
-                lowerdoor.transform.localPosition = -transform.up * t_CurrentHeight;
+                upperdoor.transform.localPosition = Vector3.up * t_CurrentHeight;
+                lowerdoor.transform.localPosition = -Vector3.up * t_CurrentHeight;
             }
 
             if (t_RelativeTime >= 1.0f)
@@ -70,8 +70,8 @@ public class Door : MonoBehaviour {
         m_StartTime = Time.time;
         if (collider)collider.SetActive(false);
 
-        upperdoor.transform.localPosition = transform.up * m_HalfDoorLength * 0.5f;
-        lowerdoor.transform.localPosition = -transform.up * m_HalfDoorLength * 0.5f;
+        upperdoor.transform.localPosition = Vector3.zero;// transform.up * m_HalfDoorLength * 0.5f;
+        lowerdoor.transform.localPosition = Vector3.zero;//-transform.up * m_HalfDoorLength * 0.5f;
     }
     public void StartClosing()
     {
@@ -79,21 +79,21 @@ public class Door : MonoBehaviour {
         m_StartTime = Time.time;
         if (collider) collider.SetActive(true);
 
-        upperdoor.transform.localPosition = transform.up * m_HalfDoorLength * 1.5f;
-        lowerdoor.transform.localPosition = -transform.up * m_HalfDoorLength * 1.5f;
+        upperdoor.transform.localPosition = Vector3.up * m_HalfDoorLength * 1.0f;
+        lowerdoor.transform.localPosition = -Vector3.up * m_HalfDoorLength * 1.0f;
     }
     public void FinishClosing()
     {
         m_IsClosing = false;
         if (collider) collider.SetActive(true);
-        upperdoor.transform.localPosition = transform.up * m_HalfDoorLength * 0.5f;
-        lowerdoor.transform.localPosition = -transform.up * m_HalfDoorLength * 0.5f;
+        upperdoor.transform.localPosition = Vector3.zero;// transform.up * m_HalfDoorLength * 0.5f;
+        lowerdoor.transform.localPosition = Vector3.zero;//-transform.up * m_HalfDoorLength * 0.5f;
     }
     public void FinishOpening()
     {
         m_IsOpening = false;
         if (collider) collider.SetActive(false);
-        upperdoor.transform.localPosition = transform.up * m_HalfDoorLength * 1.5f;
-        lowerdoor.transform.localPosition = -transform.up * m_HalfDoorLength * 1.5f;
+        upperdoor.transform.localPosition =  Vector3.up * m_HalfDoorLength * 1.0f;
+        lowerdoor.transform.localPosition = -Vector3.up * m_HalfDoorLength * 1.0f;
     }
 }
