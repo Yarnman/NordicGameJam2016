d2052ec3906e1f21ef8fcdc50d9fada374c09814
Yan Knoop <Yan.Knoop@gradsingames.com>
2016-04-10 04:58:12 +0200

    Credits

--- Nordic16/Nordic16/Assets/Code/Cosmetics/ClearColorFader.cs
+++ Nordic16/Nordic16/Assets/Code/Cosmetics/ClearColorFader.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class ClearColorFader : MonoBehaviour {
     [SerializeField] Renderer m_C;
+    [SerializeField] Renderer m_Logo;
     [SerializeField] Color m_Color;
     [SerializeField] float m_Time;
 
@@ -19,7 +20,14 @@ public class ClearColorFader : MonoBehaviour {
 
         float t_Factor = Mathf.Clamp01(t_T / m_Time);
         m_C.material.color = Color.Lerp(m_Color, Color.white, t_Factor);
-
+        if (t_T >= 0.5f)
+        {
+            float t_Time = t_T - 0.5f;
+            t_Time /= 1.0f;
+            Color t_C = m_Logo.material.color;
+            t_C.a = Mathf.Clamp01(t_Time);
+            m_Logo.material.color = t_C;
+        }
         if (Input.anyKeyDown && t_T >= m_Time)
         {
             Application.LoadLevel(0);
